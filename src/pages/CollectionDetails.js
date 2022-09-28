27af010f6ee6c67a8bb2e3f721dd746b5f57a12b
Amit Kumar Saini <amit.saini@gaadi.com>
2022-09-28 11:32:03 +0530

    add section 2

--- src/pages/CollectionDetails.js
+++ src/pages/CollectionDetails.js
@@ -3,13 +3,15 @@ import "./CollectionDetails.css";
 import { Container } from "react-bootstrap";
 import verified from "../assets/icons/verified.svg";
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const CollectionDetails = () => {
   const { collection } = useParams(); // get the collection name from the url and do the api call to get all the details about the collection
   const dummyCollection = {
     name: collection,
     bannerImg:
-      "https://assets.fractional.art/media/collections/backgrounds/ENS.jpg",
+      "https://assets.fractional.art/media/collections/backgrounds/Zora-platform-1.jpg",
     avatar: "https://assets.fractional.art/media/placeholder.png",
   };
 
@@ -18,7 +20,9 @@ const CollectionDetails = () => {
       <div
         className="banner"
         style={{ backgroundImage: `url(${dummyCollection.bannerImg})` }}
-      ></div>
+      >
+        <div className="bg-cloak"></div>
+      </div>
       <Container className="detail">
         <div className="d-flex justify-content-between">
           <div className="d-flex align-items-center">
@@ -46,7 +50,39 @@ const CollectionDetails = () => {
             </Button>
           </div>
         </div>
-        <div></div>
+        <div className="mt-4">
+          <Row>
+            <Col md={4}>
+              <div>
+                <h4>About Collection</h4>
+                <p>
+                  Read on to learn about Zora background, cultural significance,
+                  and other key information.
+                </p>
+              </div>
+            </Col>
+            <Col md={8} className="mt-2 d-flex justify-content-end ">
+              <div className="pe-3 be">
+                <h5 className="f-14 fw-bold"># OF VAULTS</h5>
+                <p className="fs-5 fw-bold ">19</p>
+              </div>
+              <div className="px-3 be">
+                <h5 className="f-14 fw-bold">OWNERS</h5>
+                <p className="fs-5 fw-bold ">15.1K</p>
+              </div>
+              <div className="px-3 be">
+                <h5 className="f-14 fw-bold">MARKET CAP</h5>
+                <p className="fs-5 fw-bold mb-0">&#8801; 15,918.1</p>
+                <p className="text-muted fw-bold">$20,424,106.90</p>
+              </div>
+              <div className="ms-3">
+                <h5 className="f-14 fw-bold">AVG. IMPLIED VAL.</h5>
+                <p className="fs-5 fw-bold mb-0">&#8801; 936.358</p>
+                <p className="text-muted fw-bold">$20,424,106.90</p>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </Container>
     </div>
   );
